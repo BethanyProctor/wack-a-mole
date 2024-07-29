3012ad3c881b579b508e82f07bae2d4cf09c2b7f
Bethany Proctor <bethanyanneproctor@gmail.com>
2024-07-29 00:18:31 -0400

    added scoring and click event

--- mole.js
+++ mole.js
@@ -1,4 +1,7 @@
 let currentMoleTile;
+let currentPlantTile;
+let score = 0;
+let gameOver = false;
 
 window.onload = function() {
     setGame();
@@ -10,10 +13,12 @@ function setGame() {
         //creates a div tag with id 0-8
         let tile = document.createElement('div');
         tile.id = i.toString();
+        tile.addEventListener("click", selectTile);
         document.getElementById("board").appendChild(tile);
     }
 
-    setInterval(setMole, 2000); //every 2 seconds
+    setInterval(setMole, 1000); //calls the mole every 1 second
+    setInterval(setPlant, 2000); //calls the plant every 2 seconds
 }
 
 function getRandomTile() {
@@ -30,6 +35,35 @@ function setMole() {
     mole.src = './monty-mole.png';
 
     let num = getRandomTile();
+    if (currentPlantTile && currentPlantTile.id === num) {
+        return;
+    }
     currentMoleTile = document.getElementById(num);
     currentMoleTile.appendChild(mole);
+}
+
+function setPlant() {
+    if (currentPlantTile) {
+        currentPlantTile.innerHTML = "";
+    }
+
+    let plant = document.createElement("img");
+    plant.src = "./piranha-plant.png";
+
+    let num = getRandomTile();
+    if (currentMoleTile && currentMoleTile.id === num) {
+        return;
+    }
+    currentPlantTile = document.getElementById(num);
+    currentPlantTile.appendChild(plant);
+}
+
+//check if the tile clicked has the mole
+function selectTile() {
+    if (this == currentMoleTile) {
+        score += 10;
+        document.getElementById("score").innerText = score.toString();
+    } else if (this == currentPlantTile) {
+        document.getElementById("score").innerText = "GAME OVER: " + score.toString();
+    }
 }
